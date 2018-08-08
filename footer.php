c5078f5d6aeb563156478552fe8c694cdd6fda49
ʞↃ∀ɾ <thagxt@users.noreply.github.com>
2018-08-08 15:36:01 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,21 +3,20 @@
 	</footer>
 </div> <!-- closing .main -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/modules.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/app.js"></script>
 
 <?php wp_footer(); ?>
 
 <!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-Y"></script><!-- change UA-XXXXXXXXX-Y with your own. -->
 <script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-ga('create', 'UA-XXXXX-Y', 'auto'); // change UA-XXXXX-Y with your own.
-ga('send', 'pageview');
+  gtag('config', 'UA-XXXXXXXXX-Y'); // change UA-XXXXXXXXX-Y with your own.
 </script>
 <!-- End Google Analytics -->
 
