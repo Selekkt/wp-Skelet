8f3f59de22b68067da23ae01de557a5a0ddd3957
ʞↃ∀ɾ <thagxt@users.noreply.github.com>
2019-06-20 15:39:44 +0200

    Update woocommerce.php

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -194,3 +194,21 @@ function wcs_custom_get_availability( $availability, $_product ) {
 
 add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
 */
+
+// Disable all WooCommerce stylesheets
+// add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+/**
+ * Set WooCommerce image dimensions upon theme activation
+ */
+
+/*
+// Remove each style one by one
+add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
+function jk_dequeue_styles( $enqueue_styles ) {
+    unset( $enqueue_styles['woocommerce-general'] );    // Remove the gloss
+    unset( $enqueue_styles['woocommerce-layout'] );     // Remove the layout
+    unset( $enqueue_styles['woocommerce-smallscreen'] );    // Remove the smallscreen optimisation
+    return $enqueue_styles;
+}
+*/
