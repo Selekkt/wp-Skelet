a05b0363ca12c6774c66c96637089b670a0dda1f
ʞↃ∀ɾ <thagxt@users.noreply.github.com>
2019-11-18 19:02:07 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	</footer>
 </div> <!-- closing .main -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/modules.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/app.js"></script>
 
