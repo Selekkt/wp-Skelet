7eea4acf78f9f9a3ae978f436b0466cd3de7311b
Sēlekkt <selekkt@gmail.com>
2019-12-11 19:58:34 +0100

    updated to Skelet. 3

--- header.php
+++ header.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    
     <meta data-framework="Skelet." data-ver="3.0.0" data-dev="Sēlekkt. Studio">
+    
+    <title><?php wp_title( '-', true, 'right' ); ?><</title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
 
-	<title><?php wp_title( '-', true, 'right' ); ?><</title>
-	<meta name="description" content="<?php bloginfo('description'); ?>">
-
-	<link href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" rel="shortcut icon">
-	<link href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/line-awesome@1.3.0/dist/line-awesome/css/line-awesome.min.css">
 
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/skelet.css" > <!-- include Skēlet. -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/app.css"> <!-- write all of your CSS here -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/skelet.css" > <!-- include Skēlet. -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/app.css"> <!-- write all of your CSS here -->
 
 	<?php wp_head(); ?>
 </head>
