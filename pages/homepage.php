9d4df9381a2074072e977ccad61885d7d09f3aac
ʞↃ∀ɾ <thagxt@users.noreply.github.com>
2020-05-10 02:12:09 +0200

    Adding homepage
    + Homepage

---
+++ pages/homepage.php
@@ -0,0 +1,21 @@
+<?php 
+	/* Template Name: Homepage */ 
+?>
+
+<?php get_header(); ?>
+
+<main role="main">
+	<section>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<!-- To see additional archive styles, visit the /bits folder -->
+			<?php get_template_part( 'bits/loop', 'page' ); ?>
+		<?php endwhile; ?>
+			<?php skelet_page_navi(); ?>
+		<?php else : ?>
+			<?php get_template_part( 'bits/content', 'notfound' ); ?>
+		<?php endif; ?>
+	</section>
+</main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
