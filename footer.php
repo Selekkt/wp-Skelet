3a7aa4d7a5af07f40e8b2079e77ca00d10631b9b
thagxt <thagxt@users.noreply.github.com>
2021-01-19 18:15:04 +0100

    jQuery version bump
    + jQuery version bump from 3.4.1 to 3.5.1
    + Google analytics snippet changed

--- footer.php
+++ footer.php
@@ -3,20 +3,19 @@
 	</footer>
 </div> <!-- closing .main -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/modules.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/app.js"></script>
 
 <?php wp_footer(); ?>
 
 <!-- Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-Y"></script><!-- change UA-XXXXXXXXX-Y with your own. -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXX"></script><!-- change G-XXXXXXXXX with your own. -->
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
-  gtag('config', 'UA-XXXXXXXXX-Y'); // change UA-XXXXXXXXX-Y with your own.
+  gtag('config', 'G-XXXXXXXXX'); // change G-XXXXXXXXX with your own.
 </script>
 <!-- End Google Analytics -->
 
