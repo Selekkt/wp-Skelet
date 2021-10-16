5b13a57ae32aa232e585b4fa3ea4e66ad28fb57c
thagxt <thagxt@users.noreply.github.com>
2021-10-16 14:38:44 +0200

    skelet.css v update + bug fixes
    Co-Authored-By: Sēlekkt. <14300778+Selekkt@users.noreply.github.com>

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     
-    <meta data-framework="Skelet." data-ver="3.1.2" data-dev="Sēlekkt. Studio">
+    <meta name="Skelet." property="Sēlekkt. Studio" content="https://selekkt.dk/skelet/v3/">
     
     <title><?php wp_title( '-', true, 'right' ); ?></title>
     <meta name="description" content="<?php bloginfo('description'); ?>">
@@ -13,13 +12,17 @@
     <link href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" rel="shortcut icon">
     <link href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"> <!-- browse the icons: https://fontawesome.com/icons?d=listing&m=free -->
-
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/skelet.css" > <!-- include Skēlet. -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/app.css"> <!-- write all of your CSS here -->
-
 	<?php wp_head(); ?>
+
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"> 
+    <!-- browse the icons: https://fontawesome.com/icons?d=listing&m=free -->
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/skelet.css">
+    <!-- include Skēlet. -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/app.css">
+    <!-- write all of your CSS here -->
+
 </head>
 <body <?php body_class(); ?>>
 <div id="app">
@@ -31,7 +34,7 @@
 			</a>
 		</div>
 
-		<nav class="nav" role="navigation">
+		<nav class="nav">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav>
 	</header>
